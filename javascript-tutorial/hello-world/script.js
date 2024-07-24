2d06cd9c8dda077a39653ce9af303d0ccf7d3a05
Alex Julian <alexanderjohnjulian@gmail.com>
2024-07-25 04:25:33 +0800

    more tutorials for javascript

--- javascript-tutorial/hello-world/script.js
+++ javascript-tutorial/hello-world/script.js
@@ -1,13 +1,9 @@
-const items = [
-  { price: 10 },
-  { price: 20 },
-  { price: 14 },
-  { price: 1 },
-  { price: 6 },
-];
+const divWithId = document.getElementById("div-id");
 
-const total = items.reduce((sum, item) => {
-  return sum + item.price;
-}, 0);
+divWithId.style.color = "red";
 
-console.log(total);
+const divWithClass = document.getElementsByClassName("div-class");
+
+const divWithClassArray = Array.from(divWithClass);
+
+divWithClassArray.forEach((div) => (div.style.color = "green"));
