bb818d35a5322565d6b682242681d55b2051d5ca
Alex Julian <alexanderjohnjulian@gmail.com>
2024-07-27 13:27:41 +0800

    finished async and await tutorial

--- javascript-tutorial/hello-world/script.js
+++ javascript-tutorial/hello-world/script.js
@@ -1,17 +1,58 @@
-const person = {
-  name: "Alex",
-  friend: {
-    name: "Joe",
-    friend: {
-      name: "Sally",
-    },
-  },
-};
-
-function printNames(currentPerson) {
-  if (currentPerson == null) return;
-  console.log(currentPerson.name);
-  printNames(currentPerson.friend);
+function getValueWithDelay(value, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
 }
 
-printNames(person);
+function getValueWithDelayError(value, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("Error");
+    }, delay);
+  });
+}
+
+// setTimeoutPromise(250)
+//   .then((message) => {
+//     console.log(message);
+//     console.log("1");
+//     return setTimeoutPromise(250);
+//   })
+//   .then((message) => {
+//     console.log(message);
+//     console.log("2");
+//   });
+
+// async function doStuff() {
+//   try {
+//     console.log("before error");
+//     const message = await setTimeoutPromise(250);
+//     console.log(message);
+//     console.log("1");
+//     const message2 = await setTimeoutPromise(250);
+//     console.log(message2);
+//     console.log("2");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// doStuff();
+
+async function challenge() {
+  try {
+    const value = await getValueWithDelay(10, 250);
+    console.log(value);
+    const value2 = await getValueWithDelay(20, 250);
+    console.log(value2);
+    const err = await getValueWithDelayError(10, 250);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("finally");
+  }
+}
+
+challenge();
