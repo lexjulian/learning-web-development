cc630797507169cdda417175cdfa080db70cab6b
Alex Julian <alexanderjohnjulian@gmail.com>
2024-08-01 02:38:43 +0800

    remake the expense tracker

--- vanilla-javascript/expense-tracker/script.js
+++ vanilla-javascript/expense-tracker/script.js
@@ -1,53 +1,54 @@
 const form = document.querySelector("#form");
-const balance = document.querySelector("#balance");
 const amount = document.querySelector("#amount");
 const text = document.querySelector("#text");
+const balance = document.querySelector("#balance");
 const income = document.querySelector("#money-plus");
 const expense = document.querySelector("#money-minus");
 const list = document.querySelector("#list");
 const STORAGE_KEY = "EXPENSE_TRACKER-list";
 
 let totalBalance = loadStorage();
-totalBalance.forEach(addToList);
+totalBalance.forEach(addListItem);
+updateBalance(totalBalance);
 
 list.addEventListener("click", (e) => {
   if (!e.target.matches(".delete-btn")) return;
 
   const parent = e.target.closest(".list-item");
-  const todoId = parent.dataset.todoId;
+  const listId = parent.dataset.listId;
   parent.remove();
-  totalBalance = totalBalance.filter((t) => t.id !== todoId);
-
-  updateBalance();
+  totalBalance = totalBalance.filter((t) => t.id !== listId);
+  updateBalance(totalBalance);
   saveToStorage();
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   if (text.value === "" || amount.value === "") {
-    alert("Please enter a text and amount.");
+    alert("Please enter text and amount");
     return;
   }
 
   const newBalance = {
-    id: new Date().valueOf().toString(),
     name: text.value,
     amount: parseInt(amount.value),
+    id: new Date().valueOf().toString(),
   };
 
   totalBalance.push(newBalance);
   computeBalance(totalBalance);
-  updateBalance();
+  computeIncome(totalBalance);
+  computeExpense(totalBalance);
 
-  addToList(newBalance);
-  saveToStorage();
-  amount.value = "";
+  updateBalance(totalBalance);
+  addListItem(newBalance);
+  saveToStorage(totalBalance);
   text.value = "";
+  amount.value = "";
 });
 
 function formatCurrency(amount) {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("en-us", {
     style: "currency",
     currency: "USD",
   });
@@ -62,55 +63,51 @@ function computeBalance(totalBalance) {
       return arr;
     }, [])
     .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
   return sum;
 }
 
-function incomeBalance(totalBalance) {
+function computeIncome(totalBalance) {
   const income = totalBalance
     .reduce((arr, balance) => {
       arr.push(balance.amount);
       return arr;
     }, [])
-    .filter((balance) => balance > 0);
-  const incomeTotal = income.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
-  return incomeTotal;
+    .filter((balance) => balance > 0)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  return income;
 }
 
-function expenseBalance(totalBalance) {
+function computeExpense(totalBalance) {
   const expense = totalBalance
     .reduce((arr, balance) => {
       arr.push(balance.amount);
       return arr;
     }, [])
-    .filter((balance) => balance < 0);
-  const expenseTotal = expense.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
-  return expenseTotal;
+    .filter((balance) => balance < 0)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  return expense;
 }
 
-function addToList(balance) {
-  updateBalance();
-  const newList = document.createElement("li");
-  newList.className = balance.amount < 0 ? "minus" : "plus";
-  newList.classList.add("list-item");
-  newList.dataset.todoId = balance.id;
-  newList.innerHTML = `${balance.name}<span>${balance.amount}</span><button class="delete-btn">x</button>`;
-
-  list.appendChild(newList);
-}
-
-function updateBalance() {
+function updateBalance(totalBalance) {
   balance.innerText = formatCurrency(computeBalance(totalBalance));
-  income.innerText = formatCurrency(incomeBalance(totalBalance));
-  expense.innerText = formatCurrency(expenseBalance(totalBalance));
+  income.innerText = formatCurrency(computeIncome(totalBalance));
+  expense.innerText = formatCurrency(computeExpense(totalBalance));
 }
 
-function saveToStorage() {
+function addListItem(totalBalance) {
+  const newItem = document.createElement("li");
+  newItem.className = totalBalance.amount > 0 ? "plus" : "minus";
+  newItem.classList.add("list-item");
+  newItem.dataset.listId = totalBalance.id;
+  newItem.innerHTML = `${totalBalance.name}<span>${totalBalance.amount}</span><button class="delete-btn">x</button>`;
+
+  list.appendChild(newItem);
+}
+
+function saveToStorage(totalBalance) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(totalBalance));
 }
 
