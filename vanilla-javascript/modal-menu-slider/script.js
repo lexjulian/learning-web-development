2be9be090c3ec7c4f4915781e50e15b6bc08a6f9
Alex Julian <alexanderjohnjulian@gmail.com>
2024-08-05 05:19:50 +0800

    finished the last version of the modal menu slider

--- vanilla-javascript/modal-menu-slider/script.js
+++ vanilla-javascript/modal-menu-slider/script.js
@@ -1,17 +1,15 @@
-const body = document.querySelector("body");
 const toggle = document.querySelector("#toggle");
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
 const modal = document.querySelector("#modal");
 
-toggle.addEventListener("click", (e) => {
-  body.classList.toggle("show-nav");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
 });
 
-open.addEventListener("click", (e) => {
-  modal.classList.toggle("show-modal");
-});
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
 
-close.addEventListener("click", (e) => {
-  modal.classList.toggle("show-modal");
+window.addEventListener("click", (e) => {
+  e.target == modal ? modal.classList.remove("show-modal") : false;
 });
