62e0bfc5ea987d48aeaa8998f0877bf448ee7521
Alex Julian <alexanderjohnjulian@gmail.com>
2024-08-05 02:49:04 +0800

    finished the custom video player

---
+++ vanilla-javascript/custom-video-player/script.js
@@ -0,0 +1,50 @@
+const video = document.querySelector("#video");
+const play = document.querySelector("#play");
+const timestamp = document.querySelector("#timestamp");
+const progress = document.querySelector("#progress");
+const stop = document.querySelector("#stop");
+
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+  } else {
+    play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
+  }
+}
+
+function videoProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+function setProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("timeupdate", videoProgress);
+progress.addEventListener("change", setProgress);
+stop.addEventListener("click", stopVideo);
