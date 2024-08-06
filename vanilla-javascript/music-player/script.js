9ab7a3411f12f257b17146a4a5fd875a61932e3c
Alex Julian <alexanderjohnjulian@gmail.com>
2024-08-07 05:10:00 +0800

    improved script.js

--- vanilla-javascript/music-player/script.js
+++ vanilla-javascript/music-player/script.js
@@ -1,21 +1,23 @@
-const play = document.querySelector("#play");
 const audio = document.querySelector("#audio");
+const play = document.querySelector("#play");
 const musicContainer = document.querySelector("#music-container");
 const title = document.querySelector("#title");
+const cover = document.querySelector("#cover");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const cover = document.querySelector("#cover");
 const progress = document.querySelector("#progress");
 const progressContainer = document.querySelector("#progress-container");
 
 const songs = ["hey", "summer", "ukulele"];
 let songIndex = 2;
 
-function songPlaying(index) {
-  audio.src = `music/${songs[index]}.mp3`;
+playingSong(2);
+
+function playingSong(songIndex) {
+  audio.src = `music/${songs[songIndex]}.mp3`;
 }
 
-function toggleAudioStatus() {
+function toggleVideoStatus() {
   if (audio.paused) {
     showMusicContainer(audio.paused);
     audio.play();
@@ -30,58 +32,58 @@ function toggleAudioStatus() {
 function showMusicContainer(bool) {
   if (bool) {
     musicContainer.classList.add("play");
-    title.innerHTML = `${songs[songIndex]}`;
+    title.innerText = songs[songIndex];
   } else {
     musicContainer.classList.remove("play");
   }
 }
 
-function nextSong() {
-  songIndex++;
-  if (songIndex >= songs.length) {
-    songPlaying(0);
-    songIndex = 0;
-  } else {
-    songPlaying(songIndex);
-  }
-  audio.play();
-  showMusicContainer(true);
-  changeCover(songIndex);
-}
-
-function prevSong() {
-  if (songIndex === 0) {
-    songPlaying(songs.length - 1);
-    songIndex = songs.length - 1;
-  } else {
-    songPlaying(songIndex);
-    songIndex--;
-  }
-  audio.play();
-  showMusicContainer(true);
-  changeCover(songIndex);
-}
-
-function changeCover(songIndex) {
+function changeCover() {
   cover.src = `images/${songs[songIndex]}.jpg`;
 }
 
-function progressBar() {
-  const progressPercent = (audio.currentTime / audio.duration) * 100;
-  progress.style.width = `${progressPercent}%`;
+function nextSong() {
+  if (songIndex >= songs.length - 1) {
+    songIndex = 0;
+  } else {
+    songIndex++;
+  }
+  playSong();
 }
 
-function setProgress(e) {
+function prevSong() {
+  if (songIndex <= 0) {
+    songIndex = songs.length - 1;
+  } else {
+    songIndex--;
+  }
+  playSong();
+}
+
+function playSong() {
+  changeCover();
+  playingSong(songIndex);
+  showMusicContainer(true);
+  audio.play();
+}
+
+function progressBar() {
+  const progressBarPercent = (audio.currentTime / audio.duration) * 100;
+  progress.style.width = `${progressBarPercent}%`;
+  if (progressBarPercent === 100) {
+    nextSong();
+  }
+}
+
+function setProgressBar(e) {
   const progressContainerWidth = progressContainer.clientWidth;
   const clickX = e.offsetX;
   const progressPercent = (clickX / progressContainerWidth) * 100;
   audio.currentTime = (progressPercent / 100) * audio.duration;
 }
 
-play.addEventListener("click", toggleAudioStatus);
+play.addEventListener("click", toggleVideoStatus);
 next.addEventListener("click", nextSong);
 prev.addEventListener("click", prevSong);
 audio.addEventListener("timeupdate", progressBar);
-progressContainer.addEventListener("click", setProgress);
-
-songPlaying(2);
+progressContainer.addEventListener("click", setProgressBar);
