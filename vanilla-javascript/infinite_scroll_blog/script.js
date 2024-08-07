8d805d47d9005dd2e336ed62e055af2f8792caee
Alex Julian <alexanderjohnjulian@gmail.com>
2024-08-08 03:57:41 +0800

    finished infinite scroll in js

---
+++ vanilla-javascript/infinite_scroll_blog/script.js
@@ -0,0 +1,68 @@
+const postsContainer = document.querySelector("#posts-container");
+const loading = document.querySelector(".loader");
+const filter = document.querySelector("#filter");
+
+let limit = 5;
+let page = 1;
+
+showPost();
+
+async function getPost() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await res.json();
+  return data;
+}
+
+async function showPost() {
+  const posts = await getPost();
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `<div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>`;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+    setTimeout(() => {
+      page++;
+      showPost();
+    }, 300);
+  }, 1000);
+}
+
+function filterPost(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener("input", filterPost);
